a9b980ebea4163037307d9d0be061e2f1a1ca481
Uthpala Herath <ukh0001@mix.wvu.edu>
2022-01-30 22:55:36 -0500

    Added newline to relax.log in Convergence.py #2

--- Convergence.py
+++ Convergence.py
@@ -268,11 +268,11 @@ def relax(args):
     if avg_force <= ediffg:
         print("Forces are converged.")
         fi.write("EDIFFG = %f and Average force = %f\n" % (ediffg, avg_force))
-        fi.write("Forces are converged.")
+        fi.write("Forces are converged.\n")
     else:
         print("Forces are not converged.")
         fi.write("EDIFFG = %f and Average force = %f\n" % (ediffg, avg_force))
-        fi.write("Forces are not converged.")
+        fi.write("Forces are not converged.\n")
     fi.close()
 
 
